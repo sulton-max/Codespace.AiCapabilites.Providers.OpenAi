e10998c9b8ce4d962a738a2df79e6e2d5ed71ced
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-09-22 13:10:47 +0500

    Created basic integration

---
+++ src/AiCapabilities.Providers.OpenAi.Basics/Models/OpenAiApiSettings.cs
@@ -0,0 +1,6 @@
+namespace AiCapabilities.Providers.OpenAi.Basics.Models;
+
+public sealed class OpenAiApiSettings
+{
+    public string ApiKey { get; set; } = default!;
+}
---
+++ src/AiCapabilities.Providers.OpenAi.Basics/Program.cs
@@ -0,0 +1,29 @@
+﻿// create servcies collection
+
+using System.Text;
+using AiCapabilities.Providers.OpenAi.Basics;
+using AiCapabilities.Providers.OpenAi.Basics.Models;
+using Microsoft.Extensions.Configuration;
+using OpenAI.Chat;
+
+// create configuration builder
+var configuration = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+// Get configuration
+var openAiApiSettings = new OpenAiApiSettings();
+configuration.GetSection(nameof(OpenAiApiSettings)).Bind(openAiApiSettings);
+
+// Create chat completion client
+var client = new ChatClient("gpt-4o-mini", openAiApiSettings.ApiKey);
+
+// Get completion
+var chatCompletion = await client.CompleteChatAsync(new UserChatMessage("Hello there!"));
+
+// Combine completion
+var builder = new StringBuilder();
+foreach (var content in chatCompletion.Value.Content)
+    builder.Append(content.Text);
+
+Console.WriteLine($"Chat completion result - {builder}");
