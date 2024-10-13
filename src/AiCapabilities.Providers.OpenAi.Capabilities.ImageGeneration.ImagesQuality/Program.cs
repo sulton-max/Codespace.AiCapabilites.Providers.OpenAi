21fe6a748da4e750dbbe76fea6852000760224d7
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2024-10-13 06:12:01 +0500

    Added images quality example

--- src/AiCapabilities.Providers.OpenAi.Capabilities.ImageGeneration.ImagesQuality/Program.cs
+++ src/AiCapabilities.Providers.OpenAi.Capabilities.ImageGeneration.ImagesQuality/Program.cs
@@ -1,4 +1,4 @@
-﻿using AiCapabilities.Providers.OpenAi.Capabilities.ImageGeneration.Models;
+﻿using AiCapabilities.Providers.OpenAi.Capabilities.ImageGeneration.ImagesQuality.Models;
 using Microsoft.Extensions.Configuration;
 using OpenAI;
 using OpenAI.Images;
@@ -15,25 +15,45 @@ var projectDirectory = Directory.GetCurrentDirectory();
 var imagesPath = Path.Combine(projectDirectory, "..", "..", "..", "Images");
 if (!Directory.Exists(imagesPath)) Directory.CreateDirectory(imagesPath);
 
-// Create client
 var openAiClient = new OpenAIClient(openAiApiSettings.ApiKey);
 var imageGenerationClient = openAiClient.GetImageClient("dall-e-3");
 
-var prompt = "A photograph of a beautiful sunset over the city.";
+var prompt = "Cute cat looking up.";
 
+// Generating a medium quality vivid image
 var options = new ImageGenerationOptions
 {
     Quality = GeneratedImageQuality.High,
-    Size = GeneratedImageSize.W1792xH1024,
+    Size = GeneratedImageSize.W1024xH1024,
     Style = GeneratedImageStyle.Vivid,
     ResponseFormat = GeneratedImageFormat.Bytes
 };
 
-// Generating an image
 var generatedImage = await imageGenerationClient.GenerateImageAsync(prompt, options);
 var binaryData = generatedImage.Value.ImageBytes;
-var fileStream = File.OpenWrite(Path.Combine(imagesPath, "sunset.jpg"));
+var fileStream = File.OpenWrite(Path.Combine(imagesPath, Path.ChangeExtension(Guid.NewGuid().ToString(), "jpg")));
 var bytesStream = binaryData.ToStream();
+
+await bytesStream.CopyToAsync(fileStream);
+await bytesStream.FlushAsync();
+
+bytesStream.Close();
+fileStream.Close();
+
+// Generating a high quality neutral image
+options = new ImageGenerationOptions
+{
+    Quality = GeneratedImageQuality.High,
+    Size = GeneratedImageSize.W1024xH1792,
+    Style = GeneratedImageStyle.Natural,
+    ResponseFormat = GeneratedImageFormat.Bytes
+};
+
+// Generating an image
+generatedImage = await imageGenerationClient.GenerateImageAsync(prompt, options);
+binaryData = generatedImage.Value.ImageBytes;
+fileStream = File.OpenWrite(Path.Combine(imagesPath, Path.ChangeExtension(Guid.NewGuid().ToString(), "jpg")));
+bytesStream = binaryData.ToStream();
 
 // Saving the generated image
 await bytesStream.CopyToAsync(fileStream);
